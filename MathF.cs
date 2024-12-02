509691796720f7cdc6b525038f1daeeee9fc6c03
Eric Chauvin <162734842+EricsSourceCode@users.noreply.github.com>
2024-12-02 14:03:49 -0700

    Add files via upload

--- MathF.cs
+++ MathF.cs
@@ -17,10 +17,6 @@ using System;
 
 
 
-// This class is mainly for things from
-// the System.Math class.
-
-
 
 public static class MathF
 {
@@ -80,6 +76,67 @@ if( a - smallNumber > b )
 
 return true;
 }
+
+
+internal static double
+                   degreesMinutesToRadians(
+                           double Degrees,
+                           double Minutes,
+                           double Seconds )
+{
+double Deg = Degrees + (Minutes / 60.0d) +
+                   (Seconds / (60.0d * 60.0d));
+// Math.PI
+// 3.14159265358979323846
+double RadConstant = (2.0d *
+             3.14159265358979323846d) / 360.0d;
+return Deg * RadConstant;
+}
+
+
+
+internal static double DegreesToRadians(
+                             double Degrees )
+{
+double RadConstant = (2.0d *
+              3.14159265358979323846d) / 360.0d;
+return Degrees * RadConstant;
+}
+
+
+
+
+internal static double RadiansToDegrees(
+                                double Radians )
+{
+double RadConstant = 360.0d /
+           (2.0d * 3.14159265358979323846d);
+return Radians * RadConstant;
+}
+
+
+
+internal static double RightAscensionToRadians(
+                             double Hours,
+                             double Minutes,
+                             double Seconds )
+{
+// Hours, minutes and seconds, with 24
+// hours being 360 degrees.
+
+double TotalHours = Hours +
+                (Minutes / 60.0d) + (Seconds /
+                (60.0d * 60.0d));
+double Degrees = TotalHours * (360.0d / 24.0d);
+// If TotalHours was 24 then it would be
+// 24 * (360 / 24) = 360
+// If TotalHours was 12 then it would be
+// 12 * (360 / 24) = 180
+
+return DegreesToRadians( Degrees );
+}
+
+
 
 
 
